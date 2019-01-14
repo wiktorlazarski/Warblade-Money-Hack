ef9b3ed15690513d909ca2ba13ff0791d226f416
Wiktor Łazarski <wjlazarski@gmail.com>
2019-01-14 10:59:58 +0100

    Simple overriding money count works

--- main.cpp
+++ main.cpp
@@ -6,13 +6,28 @@ const std::string gameWindowTitle = "Warblade 1.34";
 
 int main(int args, char** argv)
 {
-	HWND gameWindow = FindWindowA(NULL, gameWindowTitle.c_str());
+	HWND gameWindow = FindWindowA(nullptr, gameWindowTitle.c_str());
 	
-	if (gameWindow == NULL) {
-		std::cerr << "Window not found !\n";
+	if (!gameWindow) {
+		std::cerr << "Cannot find window handler!\n";
+		return -1;
 	}
 
+	DWORD processId;
+	GetWindowThreadProcessId(gameWindow, &processId);
+	if (!processId) {
+		std::cerr << "Cannot find process id!\n";
+		return -1;
+	}
+	
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
+	if (!hProcess) {
+		std::cerr << "Cannot open process!\n";
+		return -1;
+	}
 
+	int newVal = 1000;
+	WriteProcessMemory(hProcess, (LPVOID)0x00848794, &newVal, sizeof(newVal), nullptr);
 
 	return 0;
 }
